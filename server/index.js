0dfe46002b0cf21df31cf8dfa0b46d95dc51f234
severi9 <severi.x@hotmail.com>
2024-12-01 16:09:27 +0200

    Add fetch my groups

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import movieRouter from './routers/movieRouter.js';
 import AccountDeletionRouter from './routers/AccountDeletionRouter.js';
 import ReviewRouter from './routers/ReviewRouter.js';
 import GroupMemRouter from './routers/GroupMemRouter.js';
+import MyGroupsRouter from './routers/MyGroupsRouter.js';
 
 const port = 3001;
 
@@ -19,6 +20,7 @@ app.use('/', AccountDeletionRouter);
 app.use('/', ReviewRouter);
 app.use('/movie', movieRouter)
 app.use('/', GroupMemRouter);
+app.use('/', MyGroupsRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
